1dc3bf2df513f252b35bce4347689e7a218d6f21
Kenan Salic <kenan.salic@bloomreach.com>
2021-12-08 16:18:00 +0100

    milestone 2

--- my-react-content-app/src/App.js
+++ my-react-content-app/src/App.js
@@ -13,6 +13,7 @@ function App() {
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <BrPage configuration={{
+                    path:`${window.location.pathname}${window.location.search}`,
                     endpoint: 'https://kenan.bloomreach.io/delivery/site/v1/channels/getting-started/pages',
                     httpClient: axios
                 }} mapping={{Content}}>
